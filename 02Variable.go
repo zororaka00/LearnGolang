31c49a073e303f1ef9de6088707c34be406980d1
RakaWA <rakawidhiantoro@gmail.com>
2020-04-26 01:02:38 +0700

    Learn Golang

--- 02Variable.go
+++ 02Variable.go
@@ -3,6 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	var a = "Check";
-    fmt.Println(a);
+	var a = "Check"
+	fmt.Println(a)
 }
---
+++ 03Function.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func Output(a string) {
+	fmt.Println(a)
+}
+
+func main() {
+	Output("Hello World")
+}
